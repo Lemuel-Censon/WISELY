be44eaa52f06b663048ecb39fffc464a987a1c0d
Manfred Sim <manfredsim2512@gmail.com>
2020-01-07 22:17:24 +0800

    Add BLL and DAL for User

---
+++ WISLEY/BLL/User/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WISLEY.DAL.Profile;
+
+namespace WISLEY.BLL.Profile
+{
+    public class User
+    {
+        public string userId { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string userType { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public DateTime dob { get; set; }
+        public string contactNo { get; set; }
+        public string location { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(string userId, string email, string password, string userType, string fName, string lName, DateTime dob, string contactNo, string location)
+        {
+            this.userId = userId;
+            this.email = email;
+            this.password = password;
+            this.userType = userType;
+            this.fName = fName;
+            this.lName = lName;
+            this.dob = dob;
+            this.contactNo = contactNo;
+            this.location = location;
+        }
+
+        public int AddUser()
+        {
+            UserDAO userdao = new UserDAO();
+            return userdao.Insert(this);
+        }
+    }
+}
---
+++ WISLEY/DAL/User/UserDAO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WISLEY.BLL.Profile;
+
+namespace WISLEY.DAL.Profile
+{
+    public class UserDAO
+    {
+        public int Insert(User user)
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection myConn = new SqlConnection(DBConnect);
+
+            string sqlStmt = "INSERT INTO User (userId, email, password, userType, fName, lName, dob, contactNo, location)" +
+                             "VALUES (@paraUserID, @paraEmail, @paraPassword, @paraUserType, @paraFName, @paraLName, @paraDob, @paraContactNo, @paraLocation)";
+
+            int result = 0;    // Execute NonQuery return an integer value
+            SqlCommand sqlCmd = new SqlCommand(sqlStmt, myConn);
+
+            sqlCmd.Parameters.AddWithValue("@paraUserID", user.userId);
+            sqlCmd.Parameters.AddWithValue("@paraEmail", user.email);
+            sqlCmd.Parameters.AddWithValue("@paraPassword", user.password);
+            sqlCmd.Parameters.AddWithValue("@paraUserType", user.userType);
+            sqlCmd.Parameters.AddWithValue("@paraFName", user.fName);
+            sqlCmd.Parameters.AddWithValue("@paraLName", user.lName);
+            sqlCmd.Parameters.AddWithValue("@paraDob", user.dob);
+            sqlCmd.Parameters.AddWithValue("@paraContactNo", user.contactNo);
+            sqlCmd.Parameters.AddWithValue("@paraLocation", user.location);
+
+            myConn.Open();
+            result = sqlCmd.ExecuteNonQuery();
+
+            myConn.Close();
+
+            return result;
+        }
+    }
+}
