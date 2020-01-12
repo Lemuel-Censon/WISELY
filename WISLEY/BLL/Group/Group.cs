f15f254cc202f2cfd6d544ddfecb8701df957b7a
Ryuse <leemuel01234@gmail.com>
2020-01-12 15:37:38 +0800

    Just committing csproj

--- WISLEY/BLL/Group/Group.cs
+++ WISLEY/BLL/Group/Group.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WISLEY.DAL.Group;
 
 namespace WISLEY.BLL.Group
 {
     public class Group
     {
+        public string name { get; set; }
+        public string description { get; set; }
+        public int weightage { get; set; }
+
+        public Group()
+        {
+
+        }
+
+        public Group(string Name, string Description, int Weightage)
+        {
+            name = Name;
+            description = Description;
+            weightage = Weightage;
+        }
+
+        public int addGroup()
+        {
+            GroupDAO groupDAO = new GroupDAO();
+            return groupDAO.Insert(this);
+        }
     }
 }
---
+++ WISLEY/DAL/Group/GroupDAO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+namespace WISLEY.DAL.Group
+{
+    public class GroupDAO
+    {
+        public int Insert(BLL.Group.Group group)
+        {
+           
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection myConn = new SqlConnection(DBConnect);
+
+            string sqlStmt = "INSERT INTO [Group] (name, description, weightage)" +
+                 "VALUES (@paraName, @paraDescription, @paraWeightage)";
+
+            int result = 0;    // Execute NonQuery return an integer value
+            SqlCommand sqlCmd = new SqlCommand(sqlStmt, myConn);
+
+            sqlCmd.Parameters.AddWithValue("@paraName", group.name);
+            sqlCmd.Parameters.AddWithValue("@paraDescription", group.description);
+            sqlCmd.Parameters.AddWithValue("@paraWeightage", group.weightage);
+
+            myConn.Open();
+            result = sqlCmd.ExecuteNonQuery();
+
+            myConn.Close();
+
+            return result;
+        }
+
+    }
+}
