fda4056255f8e6a4a5c516607dd03fad6adf03a9
Ryuse <leemuel01234@gmail.com>
2020-01-28 23:05:07 +0800

    Minor update

--- WISLEY/Views/Resources/viewResources.aspx.cs
+++ WISLEY/Views/Resources/viewResources.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,7 @@ namespace WISLEY.Views.Resources
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            getDirectories();
         }
 
         public BLL.Group.Group getGroupDetails()
@@ -20,6 +21,24 @@ namespace WISLEY.Views.Resources
 
             BLL.Group.Group grp = new BLL.Group.Group().getGroupByID(grpId);
             return grp;
+        }
+
+        public void getDirectories()
+        {
+            System.Diagnostics.Debug.WriteLine("in GetDirectories");
+
+            string folderPath = Server.MapPath("~/Public/uploads/groupResources/") + Request.QueryString["groupId"];
+            Directory.CreateDirectory(folderPath + "/");
+
+            foreach (var d in Directory.GetDirectories(folderPath))
+            {
+                var dir = new DirectoryInfo(d);
+                var dirName = dir.Name;
+
+                System.Diagnostics.Debug.WriteLine("dir: " + dirName);
+            }
+
+
         }
     }
 
