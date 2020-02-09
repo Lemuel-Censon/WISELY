9b750769048d179a259cbd290d2b368904e63c51
Ian Loi <ianloi382@gmail.com>
2020-02-09 22:28:33 +0800

    Revert "Schedule V3.0"
    This reverts commit dc3b6016126eef191be626209f309088c4b9477d.

--- WISLEY/Views/Masters/sidebar.Master.designer.cs
+++ WISLEY/Views/Masters/sidebar.Master.designer.cs
@@ -40,15 +40,6 @@ namespace WISLEY {
         protected global::System.Web.UI.WebControls.Repeater scheduleitems;
         
         /// <summary>
-        /// btnDelete control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton btnDelete;
-        
-        /// <summary>
         /// groupCodeTB control.
         /// </summary>
         /// <remarks>
